e082c246ecd265168bdd6a5c55700c79daf53e83
Jaseem <jaseemkunji2000@gmail.com>
2025-02-02 20:33:49 +0530

    Api key from open weather website and details are fetched

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template,redirect,url_for,flash
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'hello world'
+city_name = 'malappuram'
+API_Key =  '8ebe3193fb021551c841b7d96b418a45'
+url= f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}&units=metric'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    print('Weather is ', data['weather'][0]['description'])
+    print('Current Temperature is ',data['main']['temp'])
+    print('Current Temperature feels like ',data['main']['feels_like'])
+    print('Current humidity is ',data['main']['humidity'])
+
+
+
+
+# @app.route('/')
+# def index():
+#     return 'hello world'
+
+# if __name__ == '__main__':
+#     app.run(debug=True)

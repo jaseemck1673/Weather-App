10dc1fad179a7bbe8751ad7df6d0afde83e6d09e
Jaseem <jaseemkunji2000@gmail.com>
2025-02-03 13:40:40 +0530

    datas are fetched and showed in html

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 import requests
+from api import API_Key
 from flask import Flask, render_template,redirect,url_for,flash
 
 app = Flask(__name__)
 
-city_name = 'malappuram'
-API_Key =  '8ebe3193fb021551c841b7d96b418a45'
-url= f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}&units=metric'
-
+city_name = 'New Delhi'
+# url= f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}&units=metric'
+url = f'api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={API_Key}'
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        print('Weather is ', data['weather'][0]['description'])
+        print('Current Temperature is ',data['main']['temp'])
+        print('Current Temperature feels like ',data['main']['feels_like'])
+        print('Current humidity is ',data['main']['humidity'])
+        print('Current humidity is ',data['name'])
+        return render_template('index.html',datas=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
  
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
-    print('Weather is ', data['weather'][0]['description'])
-    print('Current Temperature is ',data['main']['temp'])
-    print('Current Temperature feels like ',data['main']['feels_like'])
-    print('Current humidity is ',data['main']['humidity'])
+# response = requests.get(url)
+# if response.status_code == 200:
+#     data = response.json()
+#     print('Weather is ', data['weather'][0]['description'])
+#     print('Current Temperature is ',data['main']['temp'])
+#     print('Current Temperature feels like ',data['main']['feels_like'])
+#     print('Current humidity is ',data['main']['humidity'])
 
 
 
